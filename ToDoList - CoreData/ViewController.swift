3277f074c6226c78f0475e3d5e978b8950bf43fd
Saijal Arora <saijalarora100@gmail.com>
2023-09-12 18:27:02 -0700

    Initial Commit

---
+++ ToDoList - CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList - CoreData
+//
+//  Created by Saijal Arora  on 2023-09-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
